d3a3effa9464e2f2aaf426ab016852585e717ce9
Rashap <rashapgoyal224@gmail.com>
2024-02-20 20:05:33 +0530

    conf.js error fixing

--- Mega Blog Project/src/conf/conf.js
+++ Mega Blog Project/src/conf/conf.js
@@ -1,10 +1,10 @@
 // this additional file is a good practice for production grade applications for .env files, here we make sure that the variables must be in a string format
 const conf={
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
-    appwritedDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwritedDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 
 }
 
